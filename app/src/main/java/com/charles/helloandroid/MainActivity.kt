e632bcbb4d26840408b9ca103f25f2fd0e8b983e
Charles Vasconcelos de Souza <charlesvsouza@hotmail.com>
2022-09-18 08:04:26 -0300

    Inicial teste 1

---
+++ app/src/main/java/com/charles/helloandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.charles.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//teste1
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
